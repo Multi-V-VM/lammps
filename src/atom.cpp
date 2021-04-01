6f0283085bf89c522092fbf827e8c32410d364c3
Carlos Segarra <carlossegarragonzalez@gmail.com>
2021-04-01 15:53:02 +0000

    removed pending todo

--- src/atom.cpp
+++ src/atom.cpp
@@ -858,9 +858,8 @@ void Atom::tag_check()
 
   if (minall < 0) error->all(FLERR,"One or more Atom IDs is negative");
   if (maxall >= MAXTAGINT) error->all(FLERR,"One or more atom IDs is too big");
-  // TODO FIXME check this error
-  //if (maxall > 0 && minall == 0)
-  //  error->all(FLERR,"One or more atom IDs is zero");
+  if (maxall > 0 && minall == 0)
+    error->all(FLERR,"One or more atom IDs is zero");
   if (maxall > 0 && tag_enable == 0)
     error->all(FLERR,"Non-zero atom IDs with atom_modify id = no");
   if (maxall == 0 && natoms && tag_enable)
