0bbf7269efe9357356e50149b52d418ab29db9b3
Carlos Segarra <carlos@carlossegarra.com>
2023-03-29 15:04:15 +0000

    faasm: include header to avoid memory shrinking

--- src/main.cpp
+++ src/main.cpp
@@ -25,6 +25,10 @@
 #include "exceptions.h"
 #endif
 
+#if defined(__faasm)
+#include <faasm/faasm.h>
+#endif
+
 using namespace LAMMPS_NS;
 
 /* ----------------------------------------------------------------------
